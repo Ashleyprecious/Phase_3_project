9f68c9cc0700b10a990fff6aa322da578ffc61b9
Ashley <ashley.precios@student.moringaschool.com>
2024-02-19 21:47:57 +0300

    seeds.py 3 commit

--- lib/seeds.py
+++ lib/seeds.py
@@ -13,4 +13,13 @@ def seed_data():
     if session.query(ModelAgency).count() > 0:
         print("Seed data already exists. Skipping.")
         return
+    
+    
+    agency1 = ModelAgency(name='Elite Models')
+    agency2 = ModelAgency(name='Top Models')
+    agency3 = ModelAgency(name='Global Talents')
+    session.add_all([agency1, agency2,agency3])
+    session.commit()
+
+
 
