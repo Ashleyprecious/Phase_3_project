f6c62305684c2c75d25e02092865999a00980fc6
Ashley <ashley.precios@student.moringaschool.com>
2024-02-19 21:24:14 +0300

    Models.py 2 commit

--- lib/models/models.py
+++ lib/models/models.py
@@ -8,3 +8,13 @@ class ModelAgency(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String)
     models = relationship('Model', back_populates='agency')
+    
+class Model(Base):
+    __tablename__ = 'model'
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    agency_id = Column(Integer, ForeignKey('model_agency.id'))
+    agency = relationship('ModelAgency', back_populates='models')
+    local_magazine = relationship('LocalMagazine', back_populates='model', uselist=False)
+    photo_shoots = Column(Integer)
